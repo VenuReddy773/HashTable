0a4b65d8d707fe7bea710ffefca35ed7b5964d2d
“VenuReddy773” <venu706@gmail.com>
2021-12-03 21:07:42 +0530

    [VenuGopalReddy] Added: Hash Table Program

---
+++ HashTables/HashTables/Hash.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTables
+{
+    public class Hash<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public Hash(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size] ;
+        }
+        protected int GetArrayposition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayposition(key);
+            LinkedList<KeyValue<K, V>> LinkedList = GetLinkedList(position);
+            foreach(KeyValue<K, V> item in LinkedList)
+            {
+                if(item.key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayposition(key);
+            LinkedList<KeyValue<K, V>> LinkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { key = key, Value = value };
+            LinkedList.AddLast(item);
+        }
+        public void Remove(K key)
+        {
+            int position = GetArrayposition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFOund = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach(KeyValue<K,V> item in linkedList)
+            {
+                if (item.key.Equals(key))
+                {
+                    itemFOund = true;
+                    foundItem = item;
+                }
+            }
+            if(itemFOund)
+            {
+                linkedList.Remove(foundItem);
+            }                
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if(linkedList ==null )
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+    }
+    public struct KeyValue<k,v>
+    {
+        public k key { get; set; }
+        public v Value { get; set; }
+    }
+}
---
+++ HashTables/HashTables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HashTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hash<string, string> hash = new Hash<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "not");
+            hash.Remove("5");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value: " +hash5);
+            string hash1 = hash.Get("1");
+            Console.WriteLine("1st index value: " + hash1);
+           
+        }
+    }
+}
